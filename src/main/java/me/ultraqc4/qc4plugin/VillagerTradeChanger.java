b404a31bd95549ebec9932c9cb64bb6e4ab64de2
Alejandro Silva <alexstimp99@gmail.com>
2022-06-08 16:46:08 -0700

    lots of prints

--- src/main/java/me/ultraqc4/qc4plugin/VillagerTradeChanger.java
+++ src/main/java/me/ultraqc4/qc4plugin/VillagerTradeChanger.java
@@ -39,17 +39,40 @@ public class VillagerTradeChanger {
 
 public void SetTrades(String[] trades, Villager villager)
 {
-    System.out.println("SET TRADES");
+    System.out.println("SET TRADES 1");
     String amountItemToSell = trades[0];//amount required to sell
-    String amountItemToBuy = trades[1];//item to buy
+    System.out.println(amountItemToSell);
+    System.out.println("SET TRADES 2");
+    String itemToSell = trades[1];//item to sell
+    System.out.println(itemToSell);
+    System.out.println("SET TRADES 3");
+    String amountItemToBuy = trades[2];//item to buy
+    System.out.println(amountItemToBuy);
+    System.out.println("SET TRADES 4");
     String itemToBuy = trades[3];//amount requried to buy
-    String itemToSell = trades[4];//item to sell
+    System.out.println(itemToBuy);
+    System.out.println("SET TRADES 5");
     ItemStack itemSell = new ItemStack(Material.getMaterial(itemToSell), Integer.parseInt(amountItemToSell));
+    System.out.println(itemSell);
+    System.out.println("SET TRADES 6");
     ItemStack itemBuy = new ItemStack(Material.getMaterial(itemToBuy), Integer.parseInt(amountItemToBuy));
+    System.out.println(itemBuy);
+    System.out.println("SET TRADES 7");
     MerchantRecipe recipe =  new MerchantRecipe(itemBuy, 1000, 10000, false,0,0,0,0);
+    System.out.println(recipe.getResult());
+    System.out.println("SET TRADES 8");
     recipe.addIngredient(itemSell);
+    System.out.println(recipe.getIngredients());
+    System.out.println("SET TRADES 9");
     List<MerchantRecipe> currentRecipes = villager.getRecipes();
+    System.out.println(currentRecipes);
+    System.out.println("SET TRADES 10");
+    ///////////////
     currentRecipes.add(recipe);
+    System.out.println(currentRecipes);
+    System.out.println("SET TRADES 11");
     villager.setRecipes(currentRecipes);
+    System.out.println(villager.getRecipes());
+    System.out.println("SET TRADES 12");
 }
 }
