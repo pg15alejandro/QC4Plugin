6e28b784b9d933c1e5238459ca8771127517e4f2
Alejandro Silva <alexstimp99@gmail.com>
2022-06-07 17:06:33 -0700

    Added team check on block break

--- src/main/java/me/ultraqc4/qc4plugin/AsignacionRazas.java
+++ src/main/java/me/ultraqc4/qc4plugin/AsignacionRazas.java
@@ -1,6 +1,33 @@
 package me.ultraqc4.qc4plugin;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.scoreboard.Team;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class AsignacionRazas extends JavaPlugin {
+public class AsignacionRazas extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+
+
+        if (block.getType() == Material.GRASS) {
+            Player player = event.getPlayer();
+
+            if (player.getScoreboard().getPlayerTeam(player) == null) {
+                event.setCancelled(true);
+            }
+        }
+    }
 }
