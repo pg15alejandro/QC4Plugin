971a8f2d207ffa7920eaadf14adc099c2f9c34db
Alejandro Silva <alexstimp99@gmail.com>
2022-06-09 15:27:51 -0700

    fixed player death??

--- src/main/java/me/ultraqc4/qc4plugin/LifesPlayers.java
+++ src/main/java/me/ultraqc4/qc4plugin/LifesPlayers.java
@@ -1,10 +1,16 @@
 package me.ultraqc4.qc4plugin;
+import org.bukkit.BanList;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Team;
+import org.bukkit.scoreboard.*;
+import sun.jvm.hotspot.debugger.posix.elf.ELFException;
+import sun.util.calendar.BaseCalendar;
 
+import javax.annotation.Nullable;
+import java.util.Date;
 import java.util.HashMap;
 
 public class LifesPlayers implements Listener {
@@ -25,16 +31,31 @@ public class LifesPlayers implements Listener {
     */
 
 
-    final HashMap<Player, Integer> deaths = new HashMap<>();
+   // final HashMap<Player, Integer> deaths = new HashMap<>();
     @EventHandler
     public void playerDeath(PlayerDeathEvent event){
         final Player playerP = event.getEntity();
 
-        deaths.put(playerP, deaths.getOrDefault(playerP,0) + 1 );
-        System.out.println("Player " + playerP.getDisplayName() + " death " + deaths + " times");
+        //deaths.put(playerP, deaths.getOrDefault(playerP,0) + 1 );
+       // System.out.println("Player " + playerP.getDisplayName() + " death " + deaths + " times");
+//        Team teamT = playerP.getScoreboard().getEntryTeam(playerP.getName());
+//        String team = teamT.getName();
 
-        Team teamT = playerP.getScoreboard().getEntryTeam(playerP.getName());
-        String team = teamT.getName();
+        Objective playerScore = playerP.getScoreboard().getObjective("Deaths");
+
+        Score score = playerScore.getScore(playerP.getName());
+        Integer scoreI = score.getScore();
+
+        if (playerP.getScoreboard().getEntryTeam(playerP.getName()).equals(Races.ORCOS.toString()) ||
+            playerP.getScoreboard().getEntryTeam(playerP.getName()).equals(Races.ELFOS.toString()))
+        {
+            if (scoreI >= 3)
+            {
+                Bukkit.getBanList(BanList.Type.NAME).addBan(playerP.getName(), "LIMITE DE MUERTES ALCANZADO", null , "UltraQC 4" );
+            }
+        }
+
+        System.out.println("ALEXCAN99 SCORE IS " + scoreI);
 
 
 
